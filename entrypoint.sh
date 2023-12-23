47adefe2ac11f15ead3f21a6546a08da1385f51f
Ikari Vargas <dark.2002ikari@gmail.com>
2023-12-23 13:04:10 -0600

    Agregamos docker y entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 . ./.env
-python3 manage.py migrate
-python3 manage.py makemigrations
-python3 manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+python3 server/manage.py migrate
+python3 server/manage.py makemigrations
+python3 server/manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; 
+User = get_user_model(); 
+User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+" | python server/manage.py shell
 echo "django superuser created"
-python3 manage.py runserver 0.0.0.0:${DJANGO_PORT}
+python3 server/manage.py runserver 0.0.0.0:${DJANGO_PORT}
